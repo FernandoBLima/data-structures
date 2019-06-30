1a2b44fd0ef9913145499afa42d6cd3ab81260cd
FBL <limabfernando@gmail.com>
2019-06-30 13:09:06 -0300

    feat: adding new structures such as hashmap

---
+++ DataStructures/HashMap/hashTable.js
@@ -0,0 +1,32 @@
+class HashTable {
+
+    constructor() {
+        this.list = {}
+    }
+
+    hash(data){
+        var hashTable = 0;
+        for(var i = 0; i < data.length; i++){
+            hashTable += data.charCodeAt(i);
+        }
+        return (hashTable) - (45);
+    }
+
+    add(value, key) {
+        var position = this.hash(key); 
+        this.list[position] = value; 
+    };
+
+    get(key) {
+        return this.list[this.hash(key)];
+    };
+
+    remove(key){
+        this.list[this.hash(key)] = null;
+    }
+
+
+}
+
+
+module.exports = HashTable;
